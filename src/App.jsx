01b449737bd3816ddbf1217d83afd3e4cb737141
cendyz <arek57823@gmail.com>
2024-11-27 19:58:59 +0100

    html mobile ok

--- src/App.jsx
+++ src/App.jsx
@@ -21,25 +21,108 @@ const App = () => {
 				/>
 			</header>
 			<main className='main'>
-				<div className='main-box'>
+				<div className='first-box'>
 					<img
 						src='src/images/logo-mastercraft.svg'
 						alt='M letter'
-						className='main-logo'
+						className='first-logo'
 					/>
-					<h1 className='main-title'>Mastercraft Bamboo Monitor Riser</h1>
-					<p className='main-text'>
+					<h1 className='first-title'>Mastercraft Bamboo Monitor Riser</h1>
+					<p className='first-text'>
 						A beautiful & handcrafted monitor stand to reduce neck and eye
 						strain.
 					</p>
 					<div className='btns'>
-						<button className='main-btn'>Back this project</button>
-						<button className='main-save'>
+						<button className='first-btn'>Back this project</button>
+						<button className='first-save'>
 							<img
 								src='src/images/icon-bookmark.svg'
 								alt='Bookmark icon'
 								className='main-save-img'
 							/>
+						</button>
+					</div>
+				</div>
+				<div className='second-box'>
+					<div className='second-box-content'>
+						<p className='second-box-content-price'>$89,914</p>
+						<p className='second-box-content-text'>of $100,000 backed</p>
+						<div className='second-box-content-line'></div>
+					</div>
+					<div className='second-box-content'>
+						<p className='second-box-content-price'>5,007</p>
+						<p className='second-box-content-text'>total backers</p>
+						<div className='second-box-content-line'></div>
+					</div>
+					<div className='second-box-content'>
+						<p className='second-box-content-price'>56</p>
+						<p className='second-box-content-text'>days left</p>
+						<div className='second-box-content-line'></div>
+					</div>
+					<div className='second-box-line'></div>
+				</div>
+				<div className='last-box'>
+					<h2 className='last-box-title'>About this project</h2>
+					<p className='last-box-text1'>
+						The Mastercraft Bamboo Monitor Riser is a sturdy and stylish
+						platform that elevates your screen to a more comfortable
+						viewing height. Placing your monitor at eye level has the
+						potential to improve your posture and make you more comfortable
+						while at work, helping you stay focused on the task at hand.
+					</p>
+					<p className='last-box-text2'>
+						Featuring artisan craftsmanship, the simplicity of design
+						creates extra desk space below your computer to allow notepads,
+						pens, and USB sticks to be stored under the stand.
+					</p>
+					<div className='last-box-inner'>
+						<h3 className='last-box-inner-title'>Bamboo Stnad</h3>
+						<p className='last-box-inner-pledge'>Pledge $25 or more</p>
+						<p className='last-box-inner-text'>
+							You get an ergonomic stand made of natural bamboo. You've
+							helped us launch our promotional campaign, and you’ll be
+							added to a special Backer member list.
+						</p>
+						<div className='last-box-inner-left'>
+							<p className='last-box-inner-left-price'>101</p>
+							<p className='last-box-inner-left-text'>left</p>
+						</div>
+						<button className='last-box-inner-left-btn'>
+							Select Reward
+						</button>
+					</div>
+					<div className='last-box-inner'>
+						<h3 className='last-box-inner-title'>Black Edition Stand</h3>
+						<p className='last-box-inner-pledge'>Pledge $25 or more</p>
+						<p className='last-box-inner-text'>
+							You get a Black Special Edition computer stand and a personal
+							thank you. You’ll be added to our Backer memmber list.
+							Shipping is included.
+						</p>
+						<div className='last-box-inner-left'>
+							<p className='last-box-inner-left-price'>64</p>
+							<p className='last-box-inner-left-text'>left</p>
+						</div>
+						<button className='last-box-inner-left-btn'>
+							Select Reward
+						</button>
+					</div>
+					<div className='last-box-inner'>
+						<h3 className='last-box-inner-title'>
+							Mahogany Special Edition
+						</h3>
+						<p className='last-box-inner-pledge'>Pledge $200 or more</p>
+						<p className='last-box-inner-text'>
+							You get two Special Edition Mahogany stands, a Backer
+							T-Shirt, and a personal thank you. You’ll be added to our
+							Backer member list. Shipping is included.
+						</p>
+						<div className='last-box-inner-left'>
+							<p className='last-box-inner-left-price'>0</p>
+							<p className='last-box-inner-left-text'>Out of Stock</p>
+						</div>
+						<button className='last-box-inner-left-btn'>
+							Select Reward
 						</button>
 					</div>
 				</div>
@@ -59,37 +142,29 @@ export default App
 //   Back this project
 //   Bookmark
 
-//   $89,914 of $100,000 backed
-//   5,007 total backers
-//   56 days left
+//   $89,914
+//
+//
 
-//   About this project
+//
 
-//   The Mastercraft Bamboo Monitor Riser is a sturdy and stylish platform that elevates your screen
-//   to a more comfortable viewing height. Placing your monitor at eye level has the potential to improve
-//   your posture and make you more comfortable while at work, helping you stay focused on the task at hand.
-
-//   Featuring artisan craftsmanship, the simplicity of design creates extra desk space below your computer
-//   to allow notepads, pens, and USB sticks to be stored under the stand.
+//
 
 //   Bamboo Stand
-//   Pledge $25 or more
-//   You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and
-//   you’ll be added to a special Backer member list.
+//
+//
 //   101 left
 //   Select Reward
 
-//   Black Edition Stand
-//   Pledge $75 or more
-//   You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer
-//   member list. Shipping is included.
+//
+//
+//
 //   64 left
 //   Select Reward
 
-//   Mahogany Special Edition
-//   Pledge $200 or more
-//   You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added
-//   to our Backer member list. Shipping is included.
+//
+//
+//
 //   0 left
 //   Out of Stock
 

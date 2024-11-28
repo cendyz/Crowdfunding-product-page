ac1848518e20e57afdcff5d394b0e0a38ff3b344
cendyz <arek57823@gmail.com>
2024-11-28 19:00:48 +0100

    mobile ez

--- src/App.jsx
+++ src/App.jsx
@@ -73,66 +73,66 @@ const App = () => {
 				</div>
 				<div className='last-box'>
 					<h2 className='last-box-title'>About this project</h2>
-					<p className='last-box-text1'>
+					<p className='last-box-text'>
 						The Mastercraft Bamboo Monitor Riser is a sturdy and stylish
 						platform that elevates your screen to a more comfortable
 						viewing height. Placing your monitor at eye level has the
 						potential to improve your posture and make you more comfortable
 						while at work, helping you stay focused on the task at hand.
 					</p>
-					<p className='last-box-text2'>
+					<p className='last-box-text'>
 						Featuring artisan craftsmanship, the simplicity of design
 						creates extra desk space below your computer to allow notepads,
 						pens, and USB sticks to be stored under the stand.
 					</p>
-					<div className='last-box-inner'>
-						<h3 className='last-box-inner-title'>Bamboo Stnad</h3>
-						<p className='last-box-inner-pledge'>Pledge $25 or more</p>
-						<p className='last-box-inner-text'>
+					<div className='last__box-inner'>
+						<h3 className='last__box-inner-title'>Bamboo Stand</h3>
+						<p className='last__box-inner-pledge'>Pledge $25 or more</p>
+						<p className='last__box-inner-text'>
 							You get an ergonomic stand made of natural bamboo. You've
 							helped us launch our promotional campaign, and you’ll be
 							added to a special Backer member list.
 						</p>
-						<div className='last-box-inner-left'>
-							<p className='last-box-inner-left-price'>101</p>
-							<p className='last-box-inner-left-text'>left</p>
+						<div className='last__box-inner-left'>
+							<p className='last__box-inner-left-price'>101</p>
+							<p className='last__box-inner-left-text'>left</p>
 						</div>
-						<button className='last-box-inner-left-btn'>
+						<button className='last__box-inner-left-btn'>
 							Select Reward
 						</button>
 					</div>
-					<div className='last-box-inner'>
-						<h3 className='last-box-inner-title'>Black Edition Stand</h3>
-						<p className='last-box-inner-pledge'>Pledge $25 or more</p>
-						<p className='last-box-inner-text'>
+					<div className='last__box-inner'>
+						<h3 className='last__box-inner-title'>Black Edition Stand</h3>
+						<p className='last__box-inner-pledge'>Pledge $25 or more</p>
+						<p className='last__box-inner-text'>
 							You get a Black Special Edition computer stand and a personal
 							thank you. You’ll be added to our Backer memmber list.
 							Shipping is included.
 						</p>
-						<div className='last-box-inner-left'>
-							<p className='last-box-inner-left-price'>64</p>
-							<p className='last-box-inner-left-text'>left</p>
+						<div className='last__box-inner-left'>
+							<p className='last__box-inner-left-price'>64</p>
+							<p className='last__box-inner-left-text'>left</p>
 						</div>
-						<button className='last-box-inner-left-btn'>
+						<button className='last__box-inner-left-btn'>
 							Select Reward
 						</button>
 					</div>
-					<div className='last-box-inner'>
-						<h3 className='last-box-inner-title'>
+					<div className='last__box-inner'>
+						<h3 className='last__box-inner-title'>
 							Mahogany Special Edition
 						</h3>
-						<p className='last-box-inner-pledge'>Pledge $200 or more</p>
-						<p className='last-box-inner-text'>
+						<p className='last__box-inner-pledge'>Pledge $200 or more</p>
+						<p className='last__box-inner-text'>
 							You get two Special Edition Mahogany stands, a Backer
 							T-Shirt, and a personal thank you. You’ll be added to our
 							Backer member list. Shipping is included.
 						</p>
-						<div className='last-box-inner-left'>
-							<p className='last-box-inner-left-price'>0</p>
-							<p className='last-box-inner-left-text'>Out of Stock</p>
+						<div className='last__box-inner-left'>
+							<p className='last__box-inner-left-price'>0</p>
+							<p className='last__box-inner-left-text'>Out of Stock</p>
 						</div>
-						<button className='last-box-inner-left-btn'>
-							Select Reward
+						<button className='last__box-inner-left-btn'>
+							Out of Stock
 						</button>
 					</div>
 				</div>

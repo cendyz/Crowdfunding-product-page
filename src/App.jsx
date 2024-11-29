dc68c1f388163de0c9b70e4f7e6cfcb6a10dafd4
cendyz <arek57823@gmail.com>
2024-11-29 17:07:44 +0100

    still css

--- src/App.jsx
+++ src/App.jsx
@@ -98,92 +98,168 @@ const App = () => {
 						creates extra desk space below your computer to allow notepads,
 						pens, and USB sticks to be stored under the stand.
 					</p>
-					<div className='last__box-inner'>
-						<h3 className='last__box-inner-title'>Bamboo Stand</h3>
-						<p className='last__box-inner-pledge'>Pledge $25 or more</p>
-						<p className='last__box-inner-text'>
+					<div className='last-box-inner'>
+						<h3 className='last-box-inner-title'>Bamboo Stand</h3>
+						<p className='last-box-inner-pledge'>Pledge $25 or more</p>
+						<p className='last-box-inner-text'>
 							You get an ergonomic stand made of natural bamboo. You've
 							helped us launch our promotional campaign, and you’ll be
 							added to a special Backer member list.
 						</p>
-						<div className='last__box-inner-left'>
-							<p className='last__box-inner-left-price'>101</p>
-							<p className='last__box-inner-left-text'>left</p>
+						<div className='last-box-inner-left'>
+							<p className='last-box-inner-left-price'>101</p>
+							<p className='last-box-inner-left-text'>left</p>
 						</div>
-						<button className='last__box-inner-left-btn'>
+						<button className='last-box-inner-left-btn'>
 							Select Reward
 						</button>
 					</div>
-					<div className='last__box-inner'>
-						<h3 className='last__box-inner-title'>Black Edition Stand</h3>
-						<p className='last__box-inner-pledge'>Pledge $25 or more</p>
-						<p className='last__box-inner-text'>
+					<div className='last-box-inner'>
+						<h3 className='last-box-inner-title'>Black Edition Stand</h3>
+						<p className='last-box-inner-pledge'>Pledge $25 or more</p>
+						<p className='last-box-inner-text'>
 							You get a Black Special Edition computer stand and a personal
 							thank you. You’ll be added to our Backer memmber list.
 							Shipping is included.
 						</p>
-						<div className='last__box-inner-left'>
-							<p className='last__box-inner-left-price'>64</p>
-							<p className='last__box-inner-left-text'>left</p>
+						<div className='last-box-inner-left'>
+							<p className='last-box-inner-left-price'>64</p>
+							<p className='last-box-inner-left-text'>left</p>
 						</div>
-						<button className='last__box-inner-left-btn'>
+						<button className='last-box-inner-left-btn'>
 							Select Reward
 						</button>
 					</div>
-					<div className='last__box-inner'>
-						<h3 className='last__box-inner-title'>
+					<div className='last-box-inner'>
+						<h3 className='last-box-inner-title'>
 							Mahogany Special Edition
 						</h3>
-						<p className='last__box-inner-pledge'>Pledge $200 or more</p>
-						<p className='last__box-inner-text'>
+						<p className='last-box-inner-pledge'>Pledge $200 or more</p>
+						<p className='last-box-inner-text'>
 							You get two Special Edition Mahogany stands, a Backer
 							T-Shirt, and a personal thank you. You’ll be added to our
 							Backer member list. Shipping is included.
 						</p>
-						<div className='last__box-inner-left'>
-							<p className='last__box-inner-left-price'>0</p>
-							<p className='last__box-inner-left-text'>Out of Stock</p>
+						<div className='last-box-inner-left'>
+							<p className='last-box-inner-left-price'>0</p>
+							<p className='last-box-inner-left-text'>Out of Stock</p>
 						</div>
-						<button className='last__box-inner-left-btn'>
+						<button className='last-box-inner-left-btn'>
 							Out of Stock
 						</button>
 					</div>
 				</div>
 				<div className='last-box modal'>
 					<div className='modal-up-box'>
-						<h2 className='last-box-title'>Back this project</h2>
+						<h2 className='modal-box-title'>Back this project</h2>
 						<img
 							src='src/images/icon-close-modal.svg'
 							alt='X icon'
 							className='modal-up-box-x'
 						/>
 					</div>
-					<p className='last__box-inner-text'>
+					<p className='modal-box-text'>
 						Want to support us in bringing Mastercraft Bamboo Monitor Riser
 						out in the world?
 					</p>
-					<div className='last__box-inner'>
+					<div className='modal-box-inner'>
 						<div className='modal-select'>
-							<input type='radio' name='pledge' id='zero' className='modal-input'/>
-							<label
-								htmlFor='zero'
-								className='modal-select-label last__box-inner-title'>
-								Bamboo Stand
-								<span className='modal-pledge'>
+							<input
+								type='radio'
+								name='pledge'
+								id='zero'
+								className='modal-input'
+							/>
+							<label htmlFor='zero' className='modal-select-label'>
+								Pledge with no reward
+								<span className='modal-select-label-span'>
 									Pledge $25 or more
 								</span>
 							</label>
 						</div>
-						<h3 className='last__box-inner-title'></h3>
-						<p className=''></p>
-						<p className='last__box-inner-text'>
+						<p className='modal-box-inner-text'>
+							Choose to support us without a reward if you simply believe
+							in our project. As a backer, you will be signed up to receive
+							product updates via email.
+						</p>
+						<div className='modal-box-inner-left'>
+							<p className='modal-box-inner-left-price'>64</p>
+							<p className='modal-box-inner-left-text'>left</p>
+						</div>
+					</div>
+					<div className='modal-box-inner'>
+						<div className='modal-select'>
+							<input
+								type='radio'
+								name='pledge'
+								id='zero'
+								className='modal-input'
+							/>
+							<label htmlFor='zero' className='modal-select-label'>
+								Bamboo Stand
+								<span className='modal-select-label-span'>
+									Pledge $25 or more
+								</span>
+							</label>
+						</div>
+						<p className='modal-box-inner-text'>
+							You get an ergonomic stand made of natural bamboo. You've
+							helped us launch our promotional campaign, and you’ll be
+							added to a special Backer member list.
+						</p>
+						<div className='modal-box-inner-left'>
+							<p className='modal-box-inner-left-price'>101</p>
+							<p className='modal-box-inner-left-text'>left</p>
+						</div>
+					</div>
+					<div className='modal-box-inner'>
+						<div className='modal-select'>
+							<input
+								type='radio'
+								name='pledge'
+								id='zero'
+								className='modal-input'
+							/>
+							<label htmlFor='zero' className='modal-select-label'>
+								Black Edition Stand
+								<span className='modal-select-label-span'>
+									Pledge $75 or more
+								</span>
+							</label>
+						</div>
+						<p className='modal-box-inner-text'>
 							You get a Black Special Edition computer stand and a personal
-							thank you. You’ll be added to our Backer memmber list.
+							thank you. You’ll be added to our Backer member list.
 							Shipping is included.
 						</p>
-						<div className='last__box-inner-left'>
-							<p className='last__box-inner-left-price'>64</p>
-							<p className='last__box-inner-left-text'>left</p>
+						<div className='modal-box-inner-left'>
+							<p className='modal-box-inner-left-price'>64</p>
+							<p className='modal-box-inner-left-text'>left</p>
+						</div>
+					</div>
+					<div className='modal-box-inner'>
+						<div className='modal-select'>
+							<input
+								type='radio'
+								name='pledge'
+								id='zero'
+								className='modal-input'
+							/>
+							<label htmlFor='zero' className='modal-select-label'>
+								Mahogany Special Edition
+								<span className='modal-select-label-span'>
+									Pledge $200 or more
+								</span>
+							</label>
+						</div>
+						<p className='modal-box-inner-text'>
+							You get two Special Edition Mahogany stands, a Backer
+							T-Shirt, and a personal thank you. You’ll be added to our
+							Backer member list. Shipping is included.
+						</p>
+						<div className='modal-box-inner-left'>
+							<p className='modal-box-inner-left-price'>0</p>
+							<p className='modal-box-inner-left-text'>left</p>
 						</div>
 					</div>
 				</div>
@@ -235,13 +311,11 @@ export default App
 //
 
 //   Pledge with no reward
-//   Choose to support us without a reward if you simply believe in our project. As a backer,
-//   you will be signed up to receive product updates via email.
+//
 
-//   Bamboo Stand
+//
 //   Pledge $25 or more
-//   You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and
-//   you’ll be added to a special Backer member list.
+//
 //   101 left
 
 //   <!-- Selected pledge start -->
@@ -250,10 +324,9 @@ export default App
 //   Continue
 //   <!-- Selected pledge end -->
 
-//   Black Edition Stand
+//
 //   Pledge $75 or more
-//   You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer
-//   member list. Shipping is included.
+//
 //   64 left
 
 //   <!-- Selected pledge start -->
@@ -262,10 +335,9 @@ export default App
 //   Continue
 //   <!-- Selected pledge end -->
 
-//   Mahogany Special Edition
+//
 //   Pledge $200 or more
-//   You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added
-//   to our Backer member list. Shipping is included.
+//
 //   0 left
 
 //   <!-- Selected pledge  start -->

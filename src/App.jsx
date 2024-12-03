20260ae8825c705d5a7951241b4969d8f831409d
cendyz <arek57823@gmail.com>
2024-12-03 10:42:57 +0100

    design DONE

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ const App = () => {
 							<button className='first-btn'>Back this project</button>
 							<button className='first-save'>
 								<img
-									src='src/images/icon-bookmark.svg'
+									src='src/images/icon-bookmark-default.svg'
 									alt='Bookmark icon'
 									className='main-save-img'
 								/>
@@ -197,12 +197,14 @@ const App = () => {
 					</p>
 					<div className='modal-box-inner-desktop'>
 						<div className='modal-up-wrapper'>
-							<input
-								type='radio'
-								name='pledge'
-								id='zero'
-								className='modal-input-desktop'
-							/>
+							<div className='input-box'>
+								<input
+									type='radio'
+									name='pledge'
+									id='zero'
+									className='modal-input-desktop'
+								/>
+							</div>
 							<div className='modal-select-desktop'>
 								<div className='modal-text-up'>
 									<label
@@ -246,20 +248,75 @@ const App = () => {
 					</div>
 					<div className='modal-box-inner-desktop'>
 						<div className='modal-up-wrapper'>
-							<input
-								type='radio'
-								name='pledge'
-								id='zero'
-								className='modal-input-desktop'
-							/>
+							<div className='input-box'>
+								<div className='input-box'>
+									<input
+										type='radio'
+										name='pledge'
+										id='one'
+										className='modal-input-desktop'
+									/>
+								</div>
+							</div>
 							<div className='modal-select-desktop'>
 								<div className='modal-text-up'>
 									<label
-										htmlFor='zero'
+										htmlFor='one'
 										className='modal-select-label-desktop'>
-										Pledge with no reward
+										Bamboo Stand
 										<span className='modal-select-label-span-desktop'>
 											Pledge $25 or more
+										</span>
+									</label>
+									<div className='modal-box-inner-left-desktop'>
+										<p className='modal-box-inner-left-price-desktop'>
+											101
+										</p>
+										<p className='modal-box-inner-left-text-desktop'>
+											left
+										</p>
+									</div>
+								</div>
+								<p className='modal-box-inner-text-desktop'>
+									You get an ergonomic stand made of natural bamboo. You've
+									helped us launch our promotional campaign, and you’ll be
+									added to a special Backer member list.
+								</p>
+							</div>
+						</div>
+						<div className='modal-bottom-wrapper'>
+							<label htmlFor='price' className='modal-bottom-label'>
+								Enter your pledge
+							</label>
+							<div className='modal-bottom-right'>
+								<input
+									type='text'
+									id='price'
+									className='modal-bottom-input'
+									value='25'
+								/>
+								<button className='modal-bottom-continue'>Continue</button>
+							</div>
+						</div>
+					</div>
+					<div className='modal-box-inner-desktop'>
+						<div className='modal-up-wrapper'>
+							<div className='input-box'>
+								<input
+									type='radio'
+									name='pledge'
+									id='two'
+									className='modal-input-desktop'
+								/>
+							</div>
+							<div className='modal-select-desktop'>
+								<div className='modal-text-up'>
+									<label
+										htmlFor='two'
+										className='modal-select-label-desktop'>
+										Black Edition Stand
+										<span className='modal-select-label-span-desktop'>
+											Pledge $75 or more
 										</span>
 									</label>
 									<div className='modal-box-inner-left-desktop'>
@@ -272,9 +329,58 @@ const App = () => {
 									</div>
 								</div>
 								<p className='modal-box-inner-text-desktop'>
-									Choose to support us without a reward if you simply
-									believe in our project. As a backer, you will be signed
-									up to receive product updates via email.
+									You get a Black Special Edition computer stand and a
+									personal thank you. You’ll be added to our Backer member
+									list. Shipping is included.
+								</p>
+							</div>
+						</div>
+						<div className='modal-bottom-wrapper'>
+							<label htmlFor='price' className='modal-bottom-label'>
+								Enter your pledge
+							</label>
+							<div className='modal-bottom-right'>
+								<input
+									type='text'
+									id='price'
+									className='modal-bottom-input'
+									value='75'
+								/>
+								<button className='modal-bottom-continue'>Continue</button>
+							</div>
+						</div>
+					</div>
+					<div className='modal-box-inner-desktop'>
+						<div className='modal-up-wrapper'>
+							<div className='input-box'>
+								<input
+									type='radio'
+									name='pledge'
+									id='three'
+									className='modal-input-desktop'
+								/>
+							</div>
+							<div className='modal-select-desktop'>
+								<div className='modal-text-up'>
+									<label
+										htmlFor='three'
+										className='modal-select-label-desktop'>
+										Mahogany Special Edition
+										<span className='modal-select-label-span-desktop'>
+											Pledge $200 or more
+										</span>
+									</label>
+									<div className='modal-box-inner-left-desktop'>
+										<p className='modal-box-inner-left-price-desktop'>0</p>
+										<p className='modal-box-inner-left-text-desktop'>
+											left
+										</p>
+									</div>
+								</div>
+								<p className='modal-box-inner-text-desktop'>
+									You get two Special Edition Mahogany stands, a Backer
+									T-Shirt, and a personal thank you. You’ll be added to our
+									Backer member list. Shipping is included.
 								</p>
 							</div>
 						</div>
@@ -297,10 +403,10 @@ const App = () => {
 							<input
 								type='radio'
 								name='pledge'
-								id='zero'
+								id='four'
 								className='modal-input'
 							/>
-							<label htmlFor='zero' className='modal-select-label'>
+							<label htmlFor='four' className='modal-select-label'>
 								Pledge with no reward
 								<span className='modal-select-label-span'>
 									Pledge $25 or more
@@ -376,7 +482,7 @@ const App = () => {
 								className='modal-input'
 							/>
 							<label htmlFor='zero' className='modal-select-label'>
-								Mahogany Special Edition
+								gany Special EMahodition
 								<span className='modal-select-label-span'>
 									Pledge $200 or more
 								</span>
